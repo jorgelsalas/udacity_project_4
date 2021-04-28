7be7e511ed4216e7ac4ca4979f6060f218b4a6c1
jorgelsalas <jorgesalch@yahoo.com>
2021-04-28 01:41:10 -0600

    Adds test for the RemindersDao

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
 import org.junit.After
 import org.junit.Test
 
@@ -25,6 +26,86 @@ import org.junit.Test
 @SmallTest
 class RemindersDaoTest {
 
-//    TODO: Add testing implementation to the RemindersDao.kt
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: RemindersDatabase
+
+    @Before
+    fun initDb() {
+        // Using an in-memory database so that the information stored here disappears when the
+        // process is killed.
+        database = Room.inMemoryDatabaseBuilder(
+                ApplicationProvider.getApplicationContext(),
+                RemindersDatabase::class.java
+        ).allowMainThreadQueries().build()
+    }
+
+    @After
+    fun closeDb() = database.close()
+
+    @Test
+    fun remindersCanBeSavedAndRetrieved() = runBlockingTest {
+        // Given a valid reminder
+        val reminder = getSampleReminderDTO()
+
+        // When it is saved to the database
+        database.reminderDao().saveReminder(reminder)
+
+        // Then it can be retrieved
+        val loaded = database.reminderDao().getReminderById(reminder.id)
+        assertThat(loaded as ReminderDTO, Matchers.notNullValue())
+
+        // And all data is properly preserved
+        assertThat(loaded.id, `is`(reminder.id))
+        assertThat(loaded.title, `is`(reminder.title))
+        assertThat(loaded.description, `is`(reminder.description))
+        assertThat(loaded.location, `is`(reminder.location))
+        assertThat(loaded.latitude, `is`(reminder.latitude))
+        assertThat(loaded.longitude, `is`(reminder.longitude))
+    }
+
+    @Test
+    fun aListOfAllSavedRemindersCanBeRetrieved() = runBlockingTest {
+        // Given an empty database to which 3 reminders are added
+        assertThat(database.reminderDao().getReminders().size, `is`(0))
+        insertSampleReminders(3)
+
+        // When a list of all reminders is retrieved
+        val reminderList = database.reminderDao().getReminders()
+
+        // Then the list contains the same number of reminders as the database
+        assertThat(reminderList, Matchers.notNullValue())
+        assertThat(reminderList.isEmpty(), `is`(false))
+        assertThat(reminderList.size, `is`(3))
+    }
+
+    @Test
+    fun savedRemindersCanBeDeleted() = runBlockingTest {
+        // Given a database with 3 reminders
+        insertSampleReminders(3)
+
+        // When the database reminders are deleted
+        database.reminderDao().deleteAllReminders()
+
+        // Then retrieving the list of reminders returns a non null but empty list
+        val reminderListAfterDeletion = database.reminderDao().getReminders()
+        assertThat(reminderListAfterDeletion, Matchers.notNullValue())
+        assertThat(reminderListAfterDeletion.isEmpty(), `is`(true))
+    }
+
+    private fun getSampleReminderDTO() : ReminderDTO {
+        return ReminderDTO("title",
+                "description",
+                "location",
+                0.0,
+                0.0)
+    }
+
+    private suspend fun insertSampleReminders(reminderCount: Int) {
+        for (i in 1 .. reminderCount) {
+            database.reminderDao().saveReminder(getSampleReminderDTO())
+        }
+    }
 
 }
