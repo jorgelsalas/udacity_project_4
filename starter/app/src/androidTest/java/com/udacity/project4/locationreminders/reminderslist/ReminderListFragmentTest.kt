38b106c3b26869b496b354d63df2d884512a694f
jorgelsalas <jorgesalch@yahoo.com>
2021-04-29 03:26:46 -0600

    Adds navigation and error message tests.

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -9,11 +9,24 @@ import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import androidx.test.rule.ActivityTestRule
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.base.BaseViewModel
+import com.udacity.project4.locationreminders.RemindersActivity
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.savereminder.SaveReminderFragment
+import com.udacity.project4.locationreminders.savereminder.SaveReminderFragmentDirections
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
@@ -25,7 +38,35 @@ import org.mockito.Mockito.verify
 @MediumTest
 class ReminderListFragmentTest {
 
-//    TODO: test the navigation of the fragments.
 //    TODO: test the displayed data on the UI.
-//    TODO: add testing for the error messages.
+
+    private lateinit var dataSource: ReminderDataSource
+
+    @Before
+    fun initDataSource() {
+        dataSource = FakeDataSource()
+    }
+
+    @After
+    fun cleanupDataSource() = runBlockingTest {
+        dataSource.deleteAllReminders()
+    }
+
+    @Test
+    fun clickingTheAddReminderFABOpensTheSaveReminderFragment() {
+        // Given the user is on the ReminderListFragment
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        // When the add reminder fab button is clicked
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        // Then the user is taken to the SaveReminderFragment
+        verify(navController).navigate(ReminderListFragmentDirections.toSaveReminder())
+    }
+
+
 }
---
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragmentTest.kt
@@ -0,0 +1,128 @@
+package com.udacity.project4.locationreminders.savereminder
+
+import org.junit.Assert.*
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import androidx.test.rule.ActivityTestRule
+import com.google.android.material.R.id.snackbar_text
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.R
+import com.udacity.project4.R.string.err_enter_title
+import com.udacity.project4.R.string.err_select_location
+import com.udacity.project4.base.BaseViewModel
+import com.udacity.project4.locationreminders.RemindersActivity
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.savereminder.SaveReminderFragment
+import com.udacity.project4.locationreminders.savereminder.SaveReminderFragmentDirections
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorFragment
+import com.udacity.project4.utils.EspressoIdlingResource
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.*
+
+@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+//UI Testing
+@MediumTest
+class SaveReminderFragmentTest {
+
+    // An Idling Resource that waits for Data Binding to have no pending bindings.
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    /**
+     * Idling resources tell Espresso that the app is idle or busy. This is needed when operations
+     * are not scheduled in the main Looper (for example when executed on a different thread).
+     */
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    /**
+     * Unregister your idling resource so it can be garbage collected and does not leak any memory.
+     */
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    @Test
+    fun clickingTheFABOpensTheSaveReminderFragment() {
+        // Given the user is on the SaveReminderFragment
+        val scenario = launchFragmentInContainer<SaveReminderFragment>(Bundle(), R.style.AppTheme)
+        dataBindingIdlingResource.monitorFragment(scenario)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        // When the selectLocation button is clicked
+        onView(withId(R.id.selectLocation)).perform(click())
+
+        // Then the user is taken to the SelectLocationFragment
+        verify(navController).navigate(SaveReminderFragmentDirections.actionSaveReminderFragmentToSelectLocationFragment())
+    }
+
+    // TODO: Ask why this test is flaky
+    @Test
+    fun attemptingToSaveAReminderWithoutTitleDisplaysAnErrorMessage() {
+        // Given the user is on the SaveReminderFragment
+        val scenario = launchFragmentInContainer<SaveReminderFragment>(Bundle(), R.style.AppTheme)
+        dataBindingIdlingResource.monitorFragment(scenario)
+        onView(withId(R.id.saveReminder)).check(matches(isDisplayed()))
+
+        //onView(withId(R.id.reminderTitle)).perform(replaceText("Title"))
+        //onView(withId(R.id.reminderTitle)).perform(replaceText(""))
+
+        // When the saveReminder button is clicked
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        // An error message is shown requesting that a titled be entered
+        onView(withText(err_enter_title)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun attemptingToSaveAReminderWithoutLocationDisplaysAnErrorMessage() {
+        // Given the user is on the SaveReminderFragment and has entered a reminder title
+        val scenario = launchFragmentInContainer<SaveReminderFragment>(Bundle(), R.style.AppTheme)
+        dataBindingIdlingResource.monitorFragment(scenario)
+
+        onView(withId(R.id.reminderTitle)).perform(replaceText("Title"))
+
+        // When the saveReminder button is clicked
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        // An error message is shown requesting that a titled be entered
+        onView(withText(err_select_location)).check(matches(isDisplayed()))
+    }
+
+
+}
